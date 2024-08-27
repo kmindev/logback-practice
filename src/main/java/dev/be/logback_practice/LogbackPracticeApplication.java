70ba15812f01de8c11bc4dbf20abe605bb41bd9a
kmindev <kangkm0324@gmail.com>
2024-08-27 13:22:22 +0900

    스프링 부트 프로젝트 3.3 생성

---
+++ src/main/java/dev/be/logback_practice/LogbackPracticeApplication.java
@@ -0,0 +1,13 @@
+package dev.be.logback_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/be/logback_practice/LogbackPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.logback_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
